0368ac6762b681ddbbbfec3ecaa34c101cc1cc20
songjiang11 <lsft@DESKTOP-VT09KH4>
2020-07-29 16:41:37 +0800

    基础建设

---
+++ Web/Learn.Web/Areas/SystemManage/Controllers/ModuleController.cs
@@ -0,0 +1,22 @@
+﻿using Learn.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Learn.Web.Areas.SystemManage.Controllers
+{
+    [Area("SystemManage")]
+    public class ModuleController : BaseController
+    {
+        #region 视图
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Form()
+        {
+            return View();
+        } 
+        #endregion
+    }
+}
