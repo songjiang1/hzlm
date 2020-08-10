29a8146a0ce26f06021b68389eb5f94627dcec50
songjiang11 <lsft@DESKTOP-VT09KH4>
2020-08-10 21:53:28 +0800

    功能菜单

--- Web/Learn.Web/wwwroot/lib/layui-extend/tableTree/treeTable.js
+++ Web/Learn.Web/wwwroot/lib/layui-extend/tableTree/treeTable.js
@@ -1,13 +1,13 @@
-layui.define(['jquery'], function(exports) {
+layui.define(['jquery'], function (exports) {
 	var MOD_NAME = 'treeTable',
 		o = layui.jquery,
-		tree = function() {};
-	tree.prototype.config = function() {
+		tree = function () { };
+	tree.prototype.config = function () {
 		return {
 			top_value: 0,
 			primary_key: 'id',
-			parent_key: 'parente_id',
-			primary_key_title: 'name',
+			parent_key: 'pid',
+			primary_key_title: 'title',
 			hide_class: 'layui-hide',
 			icon: {
 				open: 'layui-icon layui-icon-triangle-d',
@@ -19,17 +19,18 @@ layui.define(['jquery'], function(exports) {
 			is_click_icon: false,
 			is_checkbox: false,
 			is_cache: true,
+			is_table: true,
 		};
 	}
-	tree.prototype.template = function(e) {
+	tree.prototype.template = function (e) {
 		var t = this,
 			level = [],
 			tbody = '',
-			is_table = o('table' + e.elem).length || !(e.is_click_icon = true),
+			is_table = e.is_checkbox,//o('table' + e.elem).length || !(e.is_click_icon = true),
 			checkbox = e.is_checkbox ? '<div class="layui-unselect layui-form-checkbox cbx" lay-skin="primary"><i class="layui-icon layui-icon-ok"></i></div>' : '',
 			checked = checkbox ? checkbox.replace('cbx', 'cbx layui-form-checked') : '',
 			thead = checkbox && '<th style="width:28px;">' + (o.inArray(e.top_value, e.checked.data) > -1 ? checked : checkbox) + '</th>';
-		o.each(t.data(e, e.data), function(idx, item) {
+		o.each(t.data(e, e.data), function (idx, item) {
 			var tr = '',
 				is_checked = false,
 				hide_class = (item[e.parent_key] == e.top_value) || (item[e.parent_key] == t.cache(e, item[e.parent_key])) ? '' : e.hide_class;
@@ -37,11 +38,11 @@ layui.define(['jquery'], function(exports) {
 			item.level = level[item[e.primary_key]] = item[e.parent_key] != e.top_value ? (level[item[e.parent_key]] + 1) : 0;
 			// 设置是否为最后一级
 			item.is_end = !e.childs[item[e.primary_key]];
-			o.each(e.cols, function(index, obj) {
+			o.each(e.cols, function (index, obj) {
 				var style = '';
 				obj.width && (style += 'width:' + obj.width + ';'), obj.align && (style += 'text-align:' + obj.align + ';'), style && (style = 'style="' + style + '"');
 				// 标记设置行checkbox选中
-				if(e.is_checkbox && e.checked && o.inArray(item[e.checked.key], e.checked.data) > -1) {
+				if (e.is_checkbox && e.checked && o.inArray(item[e.checked.key], e.checked.data) > -1) {
 					is_checked = true;
 				}
 				// 第一次遍历头部的时候拼接表格头部
@@ -58,21 +59,17 @@ layui.define(['jquery'], function(exports) {
 			tbody += '<tr class="' + hide_class + '" data-id="' + item[e.primary_key] + '" data-pid="' + item[e.parent_key] + '">' + box + tr + '</tr>';
 		});
 		// 处理表树和树的赋值模板
-		if (e.data && e.data.length < 1) {
-			tbody += '<tr class="layui-none">无数据</tr>';
-		}
 		var table = is_table ? '<thead><tr data-id="' + e.top_value + '">' + thead + '</tr></thead><tbody>' + tbody + '</tbody>' : tbody.replace(/<tr/g, '<ul').replace(/tr>/g, 'ul>').replace(/<td/g, '<li').replace(/td>/g, 'li>');
-		
 		// 确认点击图标或点击列触发展开关闭
 		var click_btn = e.is_click_icon ? '[data-down] i:not(.layui-icon-ok)' : '[data-down]';
 		// 模板渲染并处理点击展开收起等功能
-		o(e.elem).html(table).off('click', click_btn).on('click', click_btn, function() {
+		o(e.elem).html(table).off('click', click_btn).on('click', click_btn, function () {
 			var tr = o(this).parents('[data-id]'),
 				td = tr.find('[data-down]'),
 				id = tr.data('id'),
 				pid = tr.data('pid'),
 				is_open = (td.find('i:not(.layui-icon-ok)').attr('class') == e.icon.close);
-			if(is_open) {
+			if (is_open) {
 				// 展开子级（子级出现、更改图标）
 				td.find('i:not(.layui-icon-ok)').attr('class', e.icon.open);
 				td.parents(e.elem).find('[data-pid=' + id + ']').removeClass(e.hide_class);
@@ -89,7 +86,7 @@ layui.define(['jquery'], function(exports) {
 				table: e.elem,
 				is_open: is_open,
 			})
-		}).off('click', '.cbx').on('click', '.cbx', function() {
+		}).off('click', '.cbx').on('click', '.cbx', function () {
 			var is_checked = o(this).toggleClass('layui-form-checked').hasClass('layui-form-checked'),
 				tr = o(this).parents('[data-id]'),
 				id = tr.data('id'),
@@ -103,7 +100,7 @@ layui.define(['jquery'], function(exports) {
 				table: e.elem,
 				is_checked: is_checked,
 			})
-		}).off('click', '[lay-filter]').on('click', '[lay-filter]', function() {
+		}).off('click', '[lay-filter]').on('click', '[lay-filter]', function () {
 			var tr = o(this).parents('[data-id]'),
 				id = tr.data('id'),
 				pid = tr.data('pid'),
@@ -116,30 +113,30 @@ layui.define(['jquery'], function(exports) {
 		e.end && e.end(e);
 	};
 	// 同级全部选中父级选中/同级全部取消取消父级
-	tree.prototype.parents_checkbox = function(e, pid) {
+	tree.prototype.parents_checkbox = function (e, pid) {
 		var po = o(e.elem).find('[data-pid=' + pid + ']'),
 			co = o(e.elem).find('[data-id=' + pid + ']'),
 			len = o(e.elem).find('[data-pid=' + pid + '] .cbx.layui-form-checked').length;
-		if(po.length == len || len == 0) {
+		if (po.length == len || len == 0) {
 			var pid = co.data('pid');
 			len ? co.find('.cbx').addClass('layui-form-checked') : co.find('.cbx').removeClass('layui-form-checked');
 			pid === undefined || this.parents_checkbox(e, pid);
 		}
 	};
 	// 子级反选
-	tree.prototype.childs_checkbox = function(e, id, is_checked) {
+	tree.prototype.childs_checkbox = function (e, id, is_checked) {
 		var t = this;
-		o(e.elem).find('[data-pid=' + id + ']').each(function() {
+		o(e.elem).find('[data-pid=' + id + ']').each(function () {
 			var checkbox = o(this).find('.cbx');
 			is_checked ? checkbox.addClass('layui-form-checked') : checkbox.removeClass('layui-form-checked');
 			t.childs_checkbox(e, o(this).data('id'), is_checked);
 		})
 	};
 	// 点击收起循环隐藏子级元素
-	tree.prototype.childs_hide = function(e, id) {
+	tree.prototype.childs_hide = function (e, id) {
 		var t = this;
 		t.cache(e, id, false);
-		o(e.elem).find('[data-pid=' + id + ']:not(.' + e.hide_class + ')').each(function() {
+		o(e.elem).find('[data-pid=' + id + ']:not(.' + e.hide_class + ')').each(function () {
 			var td = o(this).find('[data-down]'),
 				i = td.find('i:not(.layui-icon-ok)');
 			// 关闭更换小图标
@@ -150,17 +147,12 @@ layui.define(['jquery'], function(exports) {
 		});
 	};
 	// 重新组合数据，父子级关系跟随
-	tree.prototype.data = function(e) {
+	tree.prototype.data = function (e) {
 		var lists = [],
 			childs = [];
-		if (typeof (e.data) === "object") {
-			if (e.data.code === 200) {
-				e.data = e.data.data;
-			}
-		}
-		o.each(e.data, function(idx, item) {
+		o.each(e.data, function (idx, item) {
 			lists[item[e.primary_key]] = item;
-			if(!childs[item[e.parent_key]]) {
+			if (!childs[item[e.parent_key]]) {
 				childs[item[e.parent_key]] = [];
 			}
 			childs[item[e.parent_key]][item[e.primary_key]] = item;
@@ -168,25 +160,25 @@ layui.define(['jquery'], function(exports) {
 		e.childs = childs;
 		return this.tree_data(e, lists, e.top_value, []);
 	};
-	tree.prototype.tree_data = function(e, lists, pid, data) {
+	tree.prototype.tree_data = function (e, lists, pid, data) {
 		var t = this;
-		if(lists[pid]) {
+		if (lists[pid]) {
 			data.push(lists[pid]);
 			delete lists[pid]
 		}
-		o.each(e.data, function(index, item) {
-			if(item[e.parent_key] == pid) {
+		o.each(e.data, function (index, item) {
+			if (item[e.parent_key] == pid) {
 				data.concat(t.tree_data(e, lists, item[e.primary_key], data))
 			}
 		});
 		return data;
 	};
-	tree.prototype.render = function(e) {
+	tree.prototype.render = function (e) {
 		var t = this;
 		e = o.extend(t.config(), e);
-		if(e.url) {
+		if (e.url) {
 			o.get(e.url, function (res) {
-				if (res.code === 200) { 
+				if (res.code === 200) {
 					e.data = res.data;
 				}
 				t.template(e);
@@ -197,33 +189,33 @@ layui.define(['jquery'], function(exports) {
 		return e;
 	};
 	// 获取已选值集合
-	tree.prototype.checked = function(e) {
+	tree.prototype.checked = function (e) {
 		var ids = [];
-		o(e.elem).find('.cbx.layui-form-checked').each(function() {
+		o(e.elem).find('.cbx.layui-form-checked').each(function () {
 			var id = o(this).parents('[data-id]').data('id');
 			ids.push(id);
 		})
 		return ids;
 	};
 	// 全部展开
-	tree.prototype.openAll = function(e) {
+	tree.prototype.openAll = function (e) {
 		var t = this;
-		o.each(e.data, function(idx, item) {
+		o.each(e.data, function (idx, item) {
 			item[e.primary_key] && t.cache(e, item[e.primary_key], true);
 		})
 		t.render(e);
 	}
 	// 全部关闭
-	tree.prototype.closeAll = function(e) {
+	tree.prototype.closeAll = function (e) {
 		localStorage.setItem(e.elem.substr(1), '');
 		this.render(e);
 	}
-	tree.prototype.on = function(events, callback) {
+	tree.prototype.on = function (events, callback) {
 		return layui.onevent.call(this, MOD_NAME, events, callback)
 	};
 	// 存储折叠状态
-	tree.prototype.cache = function(e, val, option) {
-		if(!e.is_cache) {
+	tree.prototype.cache = function (e, val, option) {
+		if (!e.is_cache) {
 			return false;
 		}
 		var t = this,
@@ -231,13 +223,13 @@ layui.define(['jquery'], function(exports) {
 			val = val.toString(),
 			cache = localStorage.getItem(name) ? localStorage.getItem(name).split(',') : [],
 			index = o.inArray(val, cache);
-		if(option === undefined) {
+		if (option === undefined) {
 			return index == -1 ? false : val
 		}
-		if(option && index == -1) {
+		if (option && index == -1) {
 			cache.push(val)
 		}
-		if(!option && index > -1) {
+		if (!option && index > -1) {
 			cache.splice(index, 1)
 		}
 		localStorage.setItem(name, cache.join(','));
